854b78c71f680bd39f80b5b902983d9160cbc657
Muhammed Musa <sam@overthetopseo.com>
2025-03-20 12:36:40 +0100

    crossFi config

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:3000",
   "https://work-iq.vercel.app",
-  "https://www.workiq.xyz/",
+  "https://www.workiq.xyz",
+  "https://workiq.xyz",
 ];
 
 module.exports = allowedOrigins;
